a7e38f65a3f06c8775ef34174e03a39fcff614bd
Lauri Niemi <lauri.niemi@csc.fi>
2025-06-02 13:37:56 +0300

    Example outputs of the parallel exercise

--- parallel-io/hdf5-self-study/hdf5-parallel-exercise/README.md
+++ parallel-io/hdf5-self-study/hdf5-parallel-exercise/README.md
@@ -9,8 +9,38 @@ Your task is to write the arrays to a HDF5 file as a continuous, ordered dataset
 1. Initialize MPI and setup a HDF5 file for parallel writing.
 2. Initialize the data arrays as described above.
 3. Create a one-dimensional HDF5 dataspace that is exactly as big as needed to hold the combined integers (`int` type) from all processes. Not too small, not too large.
-4. Use hyperslabs and `H5Dwrite()` to write the arrays to file.
+4. Use hyperslabs and `H5Dwrite()` to write the arrays to file. The writes should be ordered by MPI rank.
 5. Remember to cleanup any HDF5 objects that you created.
+
+Examples of what the output of `h5dump` should look like on your file.
+- With 2 MPI processes:
+```c
+HDF5 "output.h5" {
+GROUP "/" {
+   DATASET "ranks" {
+      DATATYPE  H5T_STD_I32LE
+      DATASPACE  SIMPLE { ( 3 ) / ( 3 ) }
+      DATA {
+      (0): 0, 1, 1
+      }
+   }
+}
+}
+```
+- With 5 MPI processes:
+```c
+HDF5 "output.h5" {
+GROUP "/" {
+   DATASET "ranks" {
+      DATATYPE  H5T_STD_I32LE
+      DATASPACE  SIMPLE { ( 15 ) / ( 15 ) }
+      DATA {
+      (0): 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4
+      }
+   }
+}
+}
+```
 
 You may use the [parallel HDF5 sample code](../hdf5-parallel-example/) as a starting point or start from scratch.
 
--- parallel-io/hdf5-self-study/hdf5-parallel-exercise/solution/hdf5-parallel-exercise.cpp
+++ parallel-io/hdf5-self-study/hdf5-parallel-exercise/solution/hdf5-parallel-exercise.cpp
@@ -6,7 +6,6 @@
 
 int main(int argc, char** argv) {
 
-
     // Initialize MPI
     int rank, ntasks;
     MPI_Init(&argc, &argv);
@@ -16,48 +15,64 @@ int main(int argc, char** argv) {
     // Rank r will write r + 1 integers
     std::vector<int> data(rank + 1, rank);
 
+    // Property List for using MPI
     hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
     H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
 
     hid_t file = H5Fcreate(
-        "stuff.h5",        // file name
-        H5F_ACC_TRUNC,        // Truncate mode, read/write access is implied
+        "output.h5",
+        H5F_ACC_TRUNC,
         H5P_DEFAULT,        // Default creation behavior
         plist               // Non-default File Access behavior to allow MPI-IO
     );
 
-    // Calculate dataspace 1D size
+    // Calculate dataspace 1D length
     size_t dataspaceSize = 0;
     for (size_t i = 0; i < ntasks; i++) {
         dataspaceSize += (i + 1);
     }
 
+    // Create dataspace
     hsize_t dims[] = { (hsize_t) dataspaceSize };
     hid_t dataspace = H5Screate_simple(1, dims, NULL);
+    // Create dataset
     hid_t dataset = H5Dcreate(file, "ranks", H5T_NATIVE_INT, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
     // Calculate hyperslab start for this rank
     hsize_t slabStart = 0;
     for (size_t i = 0; i < rank; i++) {
-        slabStart += i + 1;
+        slabStart += (i + 1);
     }
 
     const hsize_t start[] = { slabStart };
+    // We can use block size of 1 => set number of blocks to match the number of elements this process should write
     const hsize_t count[] = { (hsize_t) (rank + 1) };
 
     herr_t status = H5Sselect_hyperslab(
-        dataspace,    // Dataspace the hyperslab is part of
-        H5S_SELECT_SET,     // Selection operation, can be used to modify existing selections. H5S_SELECT_SET just overrides any existing selection
-        start,                  // Hyperslab start
-        NULL,           // Stride
+        dataspace,
+        H5S_SELECT_SET,
+        start,
+        NULL, // Default stride
         count,
-        NULL                    // default blocking
+        NULL // Default blocking
     );
     assert(status >= 0 && "Hyperslab selection error");
 
-    hid_t memspace = H5Screate_simple(1, count, NULL);
-    H5Dwrite(dataset, H5T_NATIVE_INT, memspace, dataspace, H5P_DEFAULT, data.data());
+    // BONUS: Setup collective write
+    hid_t xfer_plist = H5Pcreate(H5P_DATASET_XFER);
+    herr_t status = H5Pset_dxpl_mpio(xfer_plist, H5FD_MPIO_COLLECTIVE);
 
+    hid_t memspace = H5Screate_simple(1, count, NULL);
+    H5Dwrite(
+        dataset,
+        H5T_NATIVE_INT,
+        memspace,
+        dataspace,
+        xfer_plist, // Bonus: collective write. Using H5P_DEFAULT here results in non-collective write
+        data.data()
+    );
+
+    // Cleanup
     H5Sclose(memspace);
     H5Dclose(dataset);
     H5Sclose(dataspace);
